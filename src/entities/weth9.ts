34a141cae3dddab742ec27546b49b67c0309def5
macemclain <dev@risq.capital>
2021-10-19 10:20:40 -0700

    add chains

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -14,5 +14,18 @@ export const WETH9: { [chainId: number]: Token } = {
   [69]: new Token(69, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
 
   [42161]: new Token(42161, '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 18, 'WETH', 'Wrapped Ether'),
-  [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether')
+  [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether'),
+
+  //BSC
+  [56]: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
+  [97]: new Token(97, '0x094616f0bdfb0b526bd735bf66eca0ad254ca81f', 18, 'WBNB', 'Wrapped BNB'),
+
+  //AVAX
+  [43114]: new Token(56, '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', 18, 'WAVAX', 'Wrapped AVAX'),
+
+  //Polygon
+  [137]: new Token(137, '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', 18, 'WMATIC', 'Wrapped MATIC'),
+  
+  //Moonriver
+  [1285]: new Token(1285, '0x98878b06940ae243284ca214f92bb71a2b032b8a', 18, 'WMOVR', 'Wrapped MOVR'),
 }
